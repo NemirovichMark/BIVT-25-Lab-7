cad93e359ccf8099e06b5b20bb06167d7fab23e3
KiraFlux <pret2titio1n@gmail.com>
2026-02-10 01:40:00 +0300

    t3

--- Lab7/Blue/Task3.cs
+++ Lab7/Blue/Task3.cs
@@ -2,77 +2,109 @@
 {
     public class Task3
     {
-        namespace Lab7.Blue
-    {
-        public class Task3
+        public struct Participant
         {
-            public struct Participant
+            readonly string name, surname;
+            int[] penalty_times;
+            int matches_played;
+
+            public readonly string Name => name;
+            public readonly string Surname => surname;
+            public readonly int[]? PenaltyTimes => penalty_times == null ? null : (int[])penalty_times.Clone();
+
+            public readonly int TotalTime
             {
-                readonly string name, surname;
-                int[] penalty_times;
-                int matches_played;
-
-                public string Name => name;
-                public string Surname => surname;
-                public int[] PenaltyTimes => penalty_times == null ? null : (int[])penalty_times.Clone();
-
-                public int TotalTime
+                get
                 {
-                    get
+                    if (penalty_times == null)
                     {
-                        if (penalty_times == null) return 0;
-                        int sum = 0;
-                        for (int i = 0; i < matches_played; i++) sum += penalty_times[i];
-                        return sum;
+                        return 0;
                     }
-                }
 
-                public bool IsExpelled
-                {
-                    get
+                    int sum = 0;
+                    for (int i = 0; i < matches_played; i += 1)
                     {
-                        if (penalty_times == null) return false;
-                        for (int i = 0; i < matches_played; i++)
-                            if (penalty_times[i] == 10) return true;
-                        return false;
+                        sum += penalty_times[i];
                     }
+
+                    return sum;
                 }
-
-                public Participant(string name, string surname)
-                {
-                    this.name = name;
-                    this.surname = surname;
-                    penalty_times = new int[30]; // максимально 30 матчей как в условии
-                    matches_played = 0;
-                }
-
-                public void PlayMatch(int time)
-                {
-                    if (time < 0 || penalty_times == null || matches_played >= penalty_times.Length)
-                        return;
-
-                    penalty_times[matches_played] = time;
-                    matches_played++;
-                }
-
-                public static void Sort(Participant[] array)
-                {
-                    if (array != null)
-                    {
-                        Array.Sort(array, (l, r) =>
-                        {
-                            // Сортировка по возрастанию TotalTime, но исключенные игроки в конец
-                            if (l.IsExpelled && !r.IsExpelled) return 1;
-                            if (!l.IsExpelled && r.IsExpelled) return -1;
-                            return l.TotalTime.CompareTo(r.TotalTime);
-                        });
-                    }
-                }
-
-                public void Print() =>
-                    System.Console.WriteLine($"Participant{{name: \"{name}\", surname: \"{surname}\", totalTime: {TotalTime}, isExpelled: {IsExpelled}}}");
             }
+
+            public readonly bool IsExpelled
+            {
+                get
+                {
+                    if (penalty_times == null) return false;
+                    for (int i = 0; i < matches_played; i += 1)
+                    {
+                        if (penalty_times[i] == 10)
+                        {
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.penalty_times = []; // пустой массив, будет расширяться
+                this.matches_played = 0;
+            }
+
+            public void PlayMatch(int time)
+            {
+                if (time < 0) return;
+
+                // Расширяем массив на 1 элемент
+                int newSize = matches_played + 1;
+                int[] newArray = new int[newSize];
+                if (matches_played > 0)
+                {
+                    Array.Copy(penalty_times, newArray, matches_played);
+                }
+
+                newArray[matches_played] = time;
+                penalty_times = newArray;
+                matches_played += 1;
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array != null)
+                {
+                    Array.Sort(array, (l, r) =>
+                    {
+                        // Сначала по возрастанию
+                        int timeCompare = l.TotalTime.CompareTo(r.TotalTime);
+                        if (timeCompare != 0)
+                        {
+                            return timeCompare;
+                        }
+
+                        // При равных исключенные идут перед неисключенными
+                        if (l.IsExpelled && !r.IsExpelled)
+                        {
+                            return -1;
+                        }
+
+                        if (!l.IsExpelled && r.IsExpelled)
+                        {
+                            return 1;
+                        }
+
+                        // В остальных случаях сохраняем порядок
+                        return 0;
+                    });
+                }
+            }
+
+            public readonly void Print() =>
+                Console.WriteLine($"Participant{{name: \"{name}\", surname: \"{surname}\", totalTime: {TotalTime}, isExpelled: {IsExpelled}}}");
         }
     }
-}
 }
