8a33a4ac1bbf908f1ba1b816d4880d47afef122c
KiraFlux <pret2titio1n@gmail.com>
2026-02-10 19:28:29 +0300

    t2: upd

--- Lab7/Blue/Task2.cs
+++ Lab7/Blue/Task2.cs
@@ -2,37 +2,52 @@
 {
     public class Task2
     {
-        readonly struct SuperMarks(uint jumps, uint judments)
+        struct ConcreteSuperBasedMarksManagerProviderAbstractFactoryObserverSystemHandler(uint jumps, uint judments)
         {
             readonly int[,] marks = new int[jumps, judments];
+            int sum_value = 0;
+            bool need_sum_calculation = true;
 
-            public readonly int JumpsTotal => this.marks.GetLength(0);
-            public readonly int JudmentsTotal => this.marks.GetLength(1);
+            public readonly int JumpsCount => this.marks.GetLength(0);
+            public readonly int JudmentsCount => this.marks.GetLength(1);
             public readonly int[,] View => (int[,])this.marks.Clone();
 
-            public readonly int Sum() => this.marks.Cast<int>().Sum();
-
-            public readonly void Apply(int[] result, int i)
+            public int Sum
             {
-                for (var j = 0; j < this.JudmentsTotal; j += 1) { this.marks[i, j] = result[j]; }
+                get
+                {
+                    if (this.need_sum_calculation)
+                    {
+                        this.sum_value = this.marks.Cast<int>().Sum();
+                        this.need_sum_calculation = false;
+                    }
+
+                    return this.sum_value;
+                }
+            }
+
+            public void Apply(int[] result, int i)
+            {
+                for (var j = 0; j < this.JudmentsCount; j += 1) { this.marks[i, j] = result[j]; }
+                this.need_sum_calculation = true;
             }
         }
 
         public struct Participant(string name, string surname)
         {
             readonly string name = name, surname = surname;
-            readonly SuperMarks super_marks = new(jumps: 2, judments: 5);
+            readonly ConcreteSuperBasedMarksManagerProviderAbstractFactoryObserverSystemHandler marks = new(jumps: 2, judments: 5);
             int jumps_done = 0;
 
             public readonly string Name => this.name;
             public readonly string Surname => this.surname;
-            public readonly int[,] Marks => this.super_marks.View;
-            public readonly int TotalScore => this.super_marks.Sum();
+            public readonly int[,] Marks => this.marks.View;
+            public readonly int TotalScore => this.marks.Sum;
 
             public void Jump(int[] result)
             {
                 if (!IsResultDestinationValid(result) || !this.CanJump) { return; }
-                this.super_marks.Apply(result, this.jumps_done);
+                this.marks.Apply(result, this.jumps_done);
                 this.jumps_done += 1;
             }
 
@@ -43,9 +58,9 @@
 
             public readonly void Print() => Console.WriteLine(this.ToString());
 
-            readonly bool IsResultDestinationValid(int[] result) => result != null && result.Length == this.super_marks.JudmentsTotal;
+            readonly bool IsResultDestinationValid(int[] result) => result != null && result.Length == this.marks.JudmentsCount;
 
-            readonly bool CanJump => this.jumps_done < this.super_marks.JumpsTotal;
+            readonly bool CanJump => this.jumps_done < this.marks.JumpsCount;
 
             override public readonly string ToString() =>
                 $"Participant{{name: \"{name}\", surname: \"{surname}\", TotalScore: {TotalScore}, CanJump: {CanJump}}}";
