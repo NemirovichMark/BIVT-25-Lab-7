950253360fb99a8de37b116eaeb2847febc8e563
KiraFlux <pret2titio1n@gmail.com>
2026-02-10 19:16:00 +0300

    t2: upd

--- Lab7/Blue/Task2.cs
+++ Lab7/Blue/Task2.cs
@@ -2,24 +2,37 @@
 {
     public class Task2
     {
-        public struct Participant(string name, string surname) // Тривиальный конструктор потому что можем
+        readonly struct SuperMarks(uint jumps, uint judments)
         {
-            const int JumpsTotal = 2;
-            const int JudmentsTotal = 5;
+            readonly int[,] marks = new int[jumps, judments];
 
+            public readonly int JumpsTotal => this.marks.GetLength(0);
+            public readonly int JudmentsTotal => this.marks.GetLength(1);
+            public readonly int[,] View => (int[,])this.marks.Clone();
+
+            public readonly int Sum() => this.marks.Cast<int>().Sum();
+
+            public readonly void Apply(int[] result, int i)
+            {
+                for (var j = 0; j < this.JudmentsTotal; j += 1) { this.marks[i, j] = result[j]; }
+            }
+        }
+
+        public struct Participant(string name, string surname)
+        {
             readonly string name = name, surname = surname;
-            readonly int[,] marks = new int[JumpsTotal, JudmentsTotal];
+            readonly SuperMarks super_marks = new(jumps: 2, judments: 5);
             int jumps_done = 0;
 
             public readonly string Name => this.name;
             public readonly string Surname => this.surname;
-            public readonly int[,] Marks => (int[,])this.marks.Clone();
-            public readonly int TotalScore => this.marks.Cast<int>().Sum();
+            public readonly int[,] Marks => this.super_marks.View;
+            public readonly int TotalScore => this.super_marks.Sum();
 
             public void Jump(int[] result)
             {
                 if (!IsResultDestinationValid(result) || !this.CanJump) { return; }
-                for (int i = 0; i < JudmentsTotal; i += 1) { this.marks[this.jumps_done, i] = result[i]; }
+                this.super_marks.Apply(result, this.jumps_done);
                 this.jumps_done += 1;
             }
 
@@ -30,12 +43,12 @@
 
             public readonly void Print() => Console.WriteLine(this.ToString());
 
+            readonly bool IsResultDestinationValid(int[] result) => result != null && result.Length == this.super_marks.JudmentsTotal;
+
+            readonly bool CanJump => this.jumps_done < this.super_marks.JumpsTotal;
+
             override public readonly string ToString() =>
                 $"Participant{{name: \"{name}\", surname: \"{surname}\", TotalScore: {TotalScore}, CanJump: {CanJump}}}";
-
-            static bool IsResultDestinationValid(int[] result) => result != null && result.Length == JudmentsTotal;
-
-            readonly bool CanJump => this.jumps_done < JumpsTotal;
         }
     }
 }
