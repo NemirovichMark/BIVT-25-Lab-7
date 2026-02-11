c0dda0ef29add817b014fa940674c99798a532db
KiraFlux <pret2titio1n@gmail.com>
2026-02-11 12:47:05 +0300

    t4

--- Lab7/Blue/Task4.cs
+++ Lab7/Blue/Task4.cs
@@ -2,5 +2,28 @@
 {
     public class Task4
     {
+        public struct Team(string name)
+        {
+            readonly string name = name;
+            public readonly string Name => this.name;
+            public readonly string Scores => null;
+
+            public readonly int TotalScore => 0;
+
+            public void PlayMatch(int result)
+            {
+
+            }
+
+            public readonly void Print() => Console.WriteLine(this.ToString()); // 0 refs -- метод такой типа: "Я никому не ужин"
+
+            override public readonly string ToString() =>
+                $"Team{{Name: \"{this.name}\", Scores: \"{this.Scores}\", TotalScore: {this.TotalScore}}}";
+        }
+
+        public struct Group
+        {
+
+        }
     }
 }
