0408be3fde6b0520196b0d4b856c17e39eba10f6
Заболотный Кирилл <88078893+ovosh2281337@users.noreply.github.com>
2026-02-28 19:17:26 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A objA = new A("Вася", "Иванов", 18);
+            Console.WriteLine(objA.Name);
+            objA.Print();
+            B objB = new B("Bася", "Пухляк", 18, "Линукс"); // Первый коструктор
+            B objBNEW = new B("Bася", "Пухляк", 18, "Линукс", true); // Второй
+            objA.Method();
+            objB.Method();
+            objBNEW.Print();
+            B var = objA as B;
+            
+        }
+    }
+    public class A
+    {
+        protected string _name;
+        protected string _surname;
+        protected int _age;
+        public string Name => _name;
+        public string Surname => _surname;
+        public int Age => _age;
+        public A(string Name, string Surname, int Age)
+        {
+            _name = Name;
+            _surname = Surname;
+            _age = Age;
+        }
+        public virtual void Method()
+        {
+            Console.WriteLine("Method in class A");
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Возраст: {_age}");
+        }
+
+    }
+    public class B : A
+    {
+        private string _word;
+        private bool _isValid;
+        public string Word => _word;
+        public bool IsValid => _isValid;
+        public B(string Name, string Surname, int Age, string Word) : base(Name, Surname, Age)
+        {
+            _word = Word;
+            _isValid = true;
+        }
+        public B(string Name, string Surname, int Age, string Word, bool IsValid) : this(Name, Surname, Age, Word)
+        {
+            _word = Word;
+            _isValid = IsValid;
+        }
+        public override void Method()
+        {
+            Console.WriteLine("Method in class B");
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Возраст: {_age}, Слово: {_word}");
+        }
+    }
+}
--- Lab7/Purple/Task4.cs
+++ Lab7/Purple/Task4.cs
@@ -2,5 +2,140 @@
 {
     public class Task4
     {
+        public struct Sportsman
+        {
+            private string _name;
+            private string _surname;
+            private double _time;
+            public string Name => _name;
+            public string Surname => _surname;
+            public double Time => _time;
+            public Sportsman(string Name, string Surname)
+            {
+                _name = Name;
+                _surname = Surname;
+                _time = 0.0;
+            }
+            public void Run(double time)
+            {
+                if (_time == 0.0)
+                {
+                    _time = time;
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {Name}");
+                Console.WriteLine($"Фамилия: {Surname}");
+                Console.WriteLine($"Время: {Time}");
+            }
+        }
+        public struct Group
+        {
+            private string _name;
+            private Sportsman[] _sportsmen;
+            public string Name => _name;
+            public Sportsman[] Sportsmen => _sportsmen;
+            public Group(string Name)
+            {
+                _name = Name;
+                _sportsmen = new Sportsman[0];
+            }
+            public Group(Group group)
+            {
+                _name = group.Name;
+                _sportsmen = new Sportsman[group.Sportsmen.Length];
+
+                for (int i = 0; i < group.Sportsmen.Length; i++)
+                {
+                    _sportsmen[i] = group.Sportsmen[i];
+                }
+            }
+            public void Add(Sportsman sportsman)
+            {
+                Sportsman[] temp = new Sportsman[_sportsmen.Length + 1];
+                for (int i = 0; i < _sportsmen.Length; i++)
+                {
+                    temp[i] = _sportsmen[i];
+                }
+                temp[_sportsmen.Length] = sportsman;
+                _sportsmen = temp;
+            }
+            public void Add(Sportsman[] sportsmen)
+            {
+                Sportsman[] temp = new Sportsman[_sportsmen.Length + sportsmen.Length];
+                for (int i = 0; i < _sportsmen.Length; i++)
+                {
+                    temp[i] = _sportsmen[i];
+                }
+                for (int j = 0; j < sportsmen.Length; j++)
+                {
+                    temp[_sportsmen.Length + j] = sportsmen[j];
+                }
+                _sportsmen = temp;
+            }
+            public void Add(Group group)
+            {
+                Add(group.Sportsmen);
+            }
+            public void Sort()
+            {
+                for (int i = 0; i < _sportsmen.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < _sportsmen.Length; j++)
+                    {
+                        if (_sportsmen[i].Time > _sportsmen[j].Time)
+                        {
+                            (_sportsmen[i], _sportsmen[j]) = (_sportsmen[j], _sportsmen[i]);
+                        }
+                    }
+                }
+            }
+            public static Group Merge(Group group1, Group group2)
+            {
+                Group finalGroup = new Group("Финалисты");
+                finalGroup._sportsmen = new Sportsman[group1.Sportsmen.Length + group2.Sportsmen.Length];
+                int i = 0;
+                int j = 0;
+                int k = 0;
+                while (i < group1.Sportsmen.Length && j < group2.Sportsmen.Length)
+                {
+                    if (group1.Sportsmen[i].Time <= group2.Sportsmen[j].Time)
+                    {
+                        finalGroup._sportsmen[k] = group1.Sportsmen[i];
+                        i++;
+                    }
+                    else
+                    {
+                        finalGroup._sportsmen[k] = group2.Sportsmen[j];
+                        j++;
+                    }
+                    k++;
+                }
+                while (i < group1.Sportsmen.Length)
+                {
+                    finalGroup._sportsmen[k] = group1.Sportsmen[i];
+                    i++;
+                    k++;
+                }
+                while (j < group2.Sportsmen.Length)
+                {
+                    finalGroup._sportsmen[k] = group2.Sportsmen[j];
+                    j++;
+                    k++;
+                }
+                return finalGroup;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Группа: {Name}");
+                Console.WriteLine("Участники:");
+                for (int i = 0; i < _sportsmen.Length; i++)
+                {
+                    _sportsmen[i].Print();
+                    Console.WriteLine();
+                }
+            }   
+        }
     }
 }
