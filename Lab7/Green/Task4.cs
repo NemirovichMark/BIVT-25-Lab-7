221be0d93b27dce8e7e6660ae90d5617b883ef42
leonovaaa1 <mariel3onova@yandex.ru>
2026-03-02 02:09:03 +0300

    Update Task4.cs

--- Lab7/Green/Task4.cs
+++ Lab7/Green/Task4.cs
@@ -1,6 +1,57 @@
-﻿namespace Lab7.Green
+namespace Lab7.Green
 {
     public class Task4
     {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _jumps;
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _jumps = new double[3];  
+            }
+            public string Name => _name;
+            public string Surname => _surname;
+            public double[] Jumps => _jumps.ToArray();
+
+
+            public double BestJump
+            {
+                get
+                {
+                    if (_jumps == null) return 0;
+                    return _jumps.Max();  
+                }
+            }
+            public void Jump(double result)
+            {
+                if (_jumps == null) return;
+                for (int i = 0; i < _jumps.Length; i++)
+                {
+                    if (_jumps[i] == 0)
+                    {
+                        _jumps[i] = result;
+                        break;
+                    }
+                }
+            }
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+                
+                Array.Sort(array, (a, b) => b.BestJump.CompareTo(a.BestJump));
+            }
+
+            
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Лучший прыжок: {BestJump}");
+            }
+
+        }
     }
 }
