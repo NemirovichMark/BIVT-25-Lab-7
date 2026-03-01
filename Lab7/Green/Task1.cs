ae7cc23a04fc1292a6537311b26afbe6f05abae0
Ver-Vina <vervina7@mail.ru>
2026-03-01 13:38:11 +0300

    Update Task1.cs

--- Lab7/Green/Task1.cs
+++ Lab7/Green/Task1.cs
@@ -2,58 +2,64 @@ using System.Xml.Linq;
 
 namespace Lab7.Green
 {
-    public class Task1
+        public class Task1
     {
         public struct Participant
         {
             //поля
             private string _surname;
-            private int _Group;
-            //private string _Trainer;
-            //private string _Result;
-            private int[] _Stars;
+            private string _group;
+            private string _trainer;
+            private double _result;
+            private bool _hasPassed;
+            
+
             //свойства
             public string Surname
             {
                 get { return _surname; } // только для чтения
             }
-            public int Group => _Group;
-            
-            public int[]Stars
-            {
-                get { }
-            }
-            public double Reiting
-            { 
-                get 
-                {
-                    if (_Stars==null|| _Stars.Length==0)
-                        return 0;
-                    double average = 0;
-                    for (int i = 0; i < _Stars.Length; i++)
-                    {
-                        average += _Stars[i];
-                    }
-                    return _Stars[0];
-                } 
-            }
+            public string Group => _group;
+            public string Trainer => _trainer;
+            public double Result =>_result;
 
-            //public double PassedTheStandard
+            private const double STANDARD_RESULT = 100;
+            private static int _passed = 0;
+            
+            public bool HasPassed => ( _result != 0 && _result <= STANDARD_RESULT);
+            //
+            public static int PassedTheStandard => _passed;
 
             //Конструктор
-            public Participant(string surname, int Group)
+
+            static Participant()
             {
-                // проинициализировать ВСЕ поля
-                _surname = surname;
-                _Group = Group;
-                _Stars = new int[0];
+               _passed = 0;
             }
+
+            public Participant(string surname, string group, string trainer)
+            {
+                _surname = surname;
+                _group = group;
+                _trainer = trainer;
+                _result = 0;
+            
+            }
+            public void Run(double result)
+            {
+                if (_result == 0 && result > 0)
+                {
+                    _result = result;
+                    if (result <= STANDARD_RESULT)
+                        _passed++;
+                }
+            }
+
 
             public void Print()
             {
-                Console.WriteLine(_surname);
+                Console.WriteLine(Surname, Group, Trainer, Result, HasPassed );
             }
         }
     }
 }
-
