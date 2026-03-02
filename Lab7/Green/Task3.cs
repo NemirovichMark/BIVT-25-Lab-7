667b00aeaa665b0273dbd90089b44d2f96dd05f9
ryzhovserg2007-eng <ryzhov.serg.2007@gmail.com>
2026-03-02 09:23:40 +0300

    Update Task3.cs

--- Lab7/Green/Task3.cs
+++ Lab7/Green/Task3.cs
@@ -9,106 +9,80 @@ namespace Lab7.Green
             private string _name;
             private string _surname;
             private int[] _marks;
-            private bool _isExpelled;
-            private int _examCount;
+            private bool _status = false;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[] Marks => _marks.ToArray();
+            public bool IsExpelled => _status;
+
+            public double AverageMark
+            {
+                get
+                {
+                    int validMarksCount = 0;
+                    double sum = 0;
+
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        if (_marks[i] > 0)
+                        {
+                            sum += _marks[i];
+                            validMarksCount++;
+                        }
+                    }
+
+                    if (validMarksCount == 0) 
+                        return 0;
+
+                    return sum / validMarksCount;
+                }
+            }
 
             public Student(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
                 _marks = new int[3];
-                _isExpelled = false;
-                _examCount = 0;
-            }
-
-            public string Name
-            {
-                get
-                {
-                    if (_name == null) return "";
-                    return _name;
-                }
-            }
-
-            public string Surname
-            {
-                get
-                {
-                    if (_surname == null) return "";
-                    return _surname;
-                }
-            }
-
-            public int[] Marks
-            {
-                get
-                {
-                    if (_marks == null) return new int[0];
-                    int[] copy = new int[_marks.Length];
-                    for (int i = 0; i < _marks.Length; i++)
-                        copy[i] = _marks[i];
-                    return copy;
-                }
-            }
-
-            public bool IsExpelled => _isExpelled;
-
-            public double AverageMark
-            {
-                get
-                {
-                    if (_marks == null) return 0;
-                    double sum = 0;
-                    int count = 0;
-                    for (int i = 0; i < _marks.Length; i++)
-                    {
-                        if (_marks[i] > 0)
-                        {
-                            sum += _marks[i];
-                            count++;
-                        }
-                    }
-                    if (count == 0) return 0;
-                    return sum / count;
-                }
             }
 
             public void Exam(int mark)
             {
-                if (_examCount >= 3) return;
-
-                _marks[_examCount] = mark;
-                _examCount++;
-
-                if (mark == 2)
+                if (_status)
+                    return;
+                
+                for (int i = 0; i < _marks.Length; i++)
                 {
-                    _isExpelled = true;
+                    if (_marks[i] == 0)
+                    {
+                        if(mark > 2)
+                        {
+                            _marks[i] = mark;
+                            break;
+                        }
+
+                        if (mark == 2)
+                        {
+                            _status = true;
+                            _marks[i] = mark;
+                            break;
+                        }
+                    }
                 }
             }
 
             public static void SortByAverageMark(Student[] array)
             {
-                for (int i = 0; i < array.Length - 1; i++)
-                {
-                    for (int j = 0; j < array.Length - 1 - i; j++)
-                    {
-                        if (array[j].AverageMark < array[j + 1].AverageMark)
-                        {
-                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
-                        }
-                    }
-                }
+                Array.Sort(array, (a, b) => b.AverageMark.CompareTo(a.AverageMark));
             }
 
             public void Print()
             {
-                string marksStr = string.Join(", ", Marks);
-                string expelledText;
-                if (_isExpelled)
-                    expelledText = "да";
-                else
-                    expelledText = "нет";
-                Console.WriteLine($"Студент: {Name} {Surname}, оценки: {marksStr}, средний балл: {AverageMark:F2}, отчислен: {expelledText}");
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Surname: {Surname}");
+                Console.WriteLine($"Marks: {Marks}");
+                Console.WriteLine($"Average mark: {AverageMark}");
+                Console.WriteLine($"Is expelled: {IsExpelled}");
             }
         }
     }
