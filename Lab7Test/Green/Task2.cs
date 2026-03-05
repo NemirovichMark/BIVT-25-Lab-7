65f69e29ed17a70407b6dd6a43fca3a1040d5a13
mitarayushka <126961692+mitarayushka@users.noreply.github.com>
2026-03-05 22:00:26 +0300

    bug fix

--- Lab7Test/Green/Task2.cs
+++ Lab7Test/Green/Task2.cs
@@ -34,3 +34,5 @@ namespace Lab7Test.Green
         {
             var type = typeof(Lab7.Green.Task2.Student);
             Assert.IsTrue(type.IsValueType, "Student должен быть структурой");
+        }
+
