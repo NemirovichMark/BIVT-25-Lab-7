0cc67f61e2e5175c7e83427478eb38a7b47cf157
NikitaRoma <90859926+NikitaRoma@users.noreply.github.com>
2026-03-08 23:50:29 +0300

    Update Task1.cs

--- Lab7/Blue/Task1.cs
+++ Lab7/Blue/Task1.cs
@@ -1,6 +1,52 @@
-﻿namespace Lab7.Blue
+using System.ComponentModel;
+
+namespace Lab7.Blue
 {
     public class Task1
     {
+        public struct Response
+        {
+            private string _name;
+            private string _surname;
+            private int _votes;
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Votes => _votes;
+
+
+
+            public Response(string name, string surname)
+            {
+                this._name = name;
+                this._surname = surname;
+
+            }
+            public int CountVotes(Response[] responses)
+            {
+                int count_votes = 0;
+                foreach (Response response in responses)
+                {
+                    if (Name == response.Name & Surname == response.Surname)
+                    {
+                        count_votes++;
+                    }
+                }
+
+                for (int i = 0; i < responses.Length; i++)
+                {
+                    if (Name == responses[i].Name & Surname == responses[i].Surname)
+                    {
+                        responses[i]._votes = count_votes;
+                    }
+                }
+
+                return Votes;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {Name} Фамилия: {Surname}\n Голоса: {Votes}\n");
+            }
+
+        }
     }
 }
