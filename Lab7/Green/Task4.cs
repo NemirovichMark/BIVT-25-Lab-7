8ad5ef81f2216f40e834c4d3641b03d69b1c81f6
Sumiya-byte <ilkapilka60555@gmail.com>
2026-03-08 16:42:01 +0300

    Update Task4.cs

--- Lab7/Green/Task4.cs
+++ Lab7/Green/Task4.cs
@@ -1,54 +1,54 @@
-﻿namespace Lab7.Green
+using System;
+using System.Linq;
+
 namespace Lab7.Green
 {
     public class Task4
-
     {
         public struct Participant
         {
             // поля
             private string _name;
             private string _surname;
-            private double [] _jumps;
+            private double[] _jumps;
 
-            //  свойства
+            // свойства
             public string Name => _name;
             public string Surname => _surname;
-            public double [] Jumps => _jumps.ToArray();
+            public double[] Jumps => _jumps.ToArray();
             public double BestJump => _jumps.Max();
-            
+
             // конструктор
-            public Participant(string name,string surname)
+            public Participant(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
-                _jumps = new double[3]; // массив из трех попыток
+                _jumps = new double[3];
             }
-            public void Jump(double result) // заполняет результат очередного
-                                            // прыжка в массиве данными.
+
+            public void Jump(double result)
             {
                 for (int i = 0; i < _jumps.Length; i++)
                 {
-                    if (_jumps[i] == 0) // если прыжка не было, то заполняем место
+                    if (_jumps[i] == 0)
                     {
-                        _jumps[i] = result; // заполняем элемент массива значением прыжка
+                        _jumps[i] = result;
                         break;
                     }
                 }
             }
-            public static void Sort(Participant[] array) // по убыванию лучшего результата спортсмена
+
+            public static void Sort(Participant[] array)
             {
-                Array.Sort(array, (a, b) => b.BestJump.CompareTo(a.BestJump)); // от большего к меньшему
+                Array.Sort(array, (a, b) => b.BestJump.CompareTo(a.BestJump));
             }
+
             public void Print()
             {
                 Console.WriteLine($"Name: {Name}");
                 Console.WriteLine($"Surname: {Surname}");
-                Console.WriteLine($"Jumps: {Jumps}");
                 Console.WriteLine($"Best jump: {BestJump}");
             }
-
         }
     }
-}
 }
