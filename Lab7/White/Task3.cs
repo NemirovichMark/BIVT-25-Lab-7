0d92ab6e17eb14ae09541927eafa7d656540087b
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-09 20:09:27 +0300

    Update Task3.cs

--- Lab7/White/Task3.cs
+++ Lab7/White/Task3.cs
@@ -2,61 +2,70 @@ using System;
 
 namespace Lab7.White
 {
-    public class Task3
+   public struct Student
+{
+    private string name;
+    private string surname;
+    private int[] marks;
+    private int count;
+    private int skipped;
+
+    public string Name => name;
+    public string Surname => surname;
+    public int Skipped => skipped;
+
+    public double AverageMark
     {
-        public struct Student
+        get
         {
-            private string name;
-            private string surname;
-            private int[] marks;
-            private int count;
-            private int skipped;
+            if (count == 0) return 0;
 
-            public string Name => name;
-            public string Surname => surname;
-            public int Skipped => skipped;
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+                sum += marks[i];
 
-            public double AverageMark
+            return (double)sum / count;
+        }
+    }
+
+    public Student(string name, string surname)
+    {
+        this.name = name ?? "";
+        this.surname = surname ?? "";
+        marks = new int[100];
+        count = 0;
+        skipped = 0;
+    }
+
+    public void Lesson(int mark)
+    {
+        if (mark == 0)
+            skipped++;
+        else
+        {
+            marks[count] = mark;
+            count++;
+        }
+    }
+
+    public static void SortBySkipped(Student[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
             {
-                get
+                if (array[i].Skipped < array[j].Skipped)
                 {
-                    if (count == 0) return 0;
-                    int sum = 0;
-                    for (int i = 0; i < count; i++) sum += marks[i];
-                    return (double)sum / count;
+                    Student temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
-            }
-
-            public Student(string name, string surname)
-            {
-                this.name = name;
-                this.surname = surname;
-                marks = new int[100];
-                count = 0;
-                skipped = 0;
-            }
-
-            public void Lesson(int mark)
-            {
-                if (mark == 0)
-                    skipped++;
-                else
-                {
-                    marks[count] = mark;
-                    count++;
-                }
-            }
-
-            // ВАЖНО: имя метода должно быть Sort, а не SortBySkipped!
-            public static void Sort(Student[] array)
-            {
-                Array.Sort(array, (a, b) => b.Skipped.CompareTo(a.Skipped));
-            }
-
-            public void Print()
-            {
-                Console.WriteLine($"{name} {surname} | Ср.балл: {AverageMark:F2} | Пропуски: {skipped}");
             }
         }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{name} {surname} Avg:{AverageMark} Skipped:{skipped}");
     }
 }
