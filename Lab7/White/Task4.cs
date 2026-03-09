3e8ffaa6ee0d9d267c3743f0479bafafbde21ee4
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-09 17:59:47 +0300

    Update Task4.cs

--- Lab7/White/Task4.cs
+++ Lab7/White/Task4.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab7.White
 {
@@ -8,26 +10,29 @@ namespace Lab7.White
         {
             private string name;
             private string surname;
-            private List<double> scores;
+            private List<double> scores;  // список для хранения результатов
 
+            // Свойства только для чтения
             public string Name => name;
             public string Surname => surname;
 
+            // СВОЙСТВО SCORES - ИСПРАВЛЕНО!
             public double[] Scores
             {
                 get
                 {
-                    if (scores == null) return null;
-                    return scores.ToArray();
+                    return scores.ToArray();  // преобразуем список в массив
+                    // scores всегда существует, так как инициализируется в конструкторе
                 }
             }
 
+            // Свойство для суммы очков
             public double TotalScore
             {
                 get
                 {
-                    if (scores == null || scores.Count == 0) return 0;
-
+                    if (scores.Count == 0) return 0;
+                    
                     double sum = 0;
                     foreach (double score in scores)
                     {
@@ -37,44 +42,31 @@ namespace Lab7.White
                 }
             }
 
+            // КОНСТРУКТОР 
             public Participant(string name, string surname)
             {
                 this.name = name;
                 this.surname = surname;
-                scores = new List<double>();
+                scores = new List<double>(); 
             }
 
+            // Метод для добавления результата
             public void PlayMatch(double result)
             {
-                if (scores == null)
-                    scores = new List<double>();
-
-                scores.Add(result);
+                scores.Add(result);  // добавляем в список
             }
 
+            // Метод сортировки по убыванию суммы
             public static void Sort(Participant[] array)
             {
-                if (array == null) return;
-
-                for (int i = 0; i < array.Length - 1; i++)
-                {
-                    for (int j = 0; j < array.Length - i - 1; j++)
-                    {
-                        if (array[j].TotalScore < array[j + 1].TotalScore)
-                        {
-                            Participant temp = array[j];
-                            array[j] = array[j + 1];
-                            array[j + 1] = temp;
-                        }
-                    }
-                }
+                Array.Sort(array, (a, b) => b.TotalScore.CompareTo(a.TotalScore));
             }
 
+            // Метод вывода
             public void Print()
             {
-                Console.WriteLine($"Name: {name}, Surname: {surname}, TotalScore: {TotalScore}");
+                Console.WriteLine($"{name} {surname} | Всего очков: {TotalScore}");
             }
         }
     }
 }
-
