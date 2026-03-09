9286a5e1ddc3267c5f26ed3064bbfbc9a92a16b1
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-09 17:29:59 +0300

    Update Task4.cs

--- Lab7/White/Task4.cs
+++ Lab7/White/Task4.cs
@@ -1,72 +1,81 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public struct Participant
+namespace Lab7.White
 {
-    private string name;
-    private string surname;
-    private double[] scores;
-    private int count;
-
-    public string Name
+    public class Task4
     {
-        get { return name; }
-    }
-
-    public string Surname
-    {
-        get { return surname; }
-    }
-
-    public double[] Scores
-    {
-        get { return scores; }
-    }
-
-    public double TotalScore
-    {
-        get
+        public struct Participant
         {
-            double sum = 0;
-            for (int i = 0; i < count; i++)
+            private string name;
+            private string surname;
+            private List<double> scores;
+
+            public string Name => name;
+            public string Surname => surname;
+
+            public double[] Scores
             {
-                sum += scores[i];
-            }
-            return sum;
-        }
-    }
-
-    public Participant(string name, string surname)
-    {
-        this.name = name;
-        this.surname = surname;
-        scores = new double[100];
-        count = 0;
-    }
-
-    public void PlayMatch(double result)
-    {
-        scores[count] = result;
-        count++;
-    }
-
-    public static void Sort(Participant[] array)
-    {
-        for (int i = 0; i < array.Length - 1; i++)
-        {
-            for (int j = i + 1; j < array.Length; j++)
-            {
-                if (array[i].TotalScore < array[j].TotalScore)
+                get
                 {
-                    Participant temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                    if (scores == null) return null;
+                    return scores.ToArray();
                 }
             }
-        }
-    }
 
-    public void Print()
-    {
-        Console.WriteLine(name + " " + surname + " | Score: " + TotalScore);
+            public double TotalScore
+            {
+                get
+                {
+                    if (scores == null || scores.Count == 0) return 0;
+
+                    double sum = 0;
+                    foreach (double score in scores)
+                    {
+                        sum += score;
+                    }
+                    return sum;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+                scores = new List<double>();
+            }
+
+            public void PlayMatch(double result)
+            {
+                if (scores == null)
+                    scores = new List<double>();
+
+                scores.Add(result);
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].TotalScore < array[j + 1].TotalScore)
+                        {
+                            Participant temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Name: {name}, Surname: {surname}, TotalScore: {TotalScore}");
+            }
+        }
     }
 }
