10889003290f8852fcb453d9f7d41adb08eb8c98
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-09 20:08:01 +0300

    Update Task2.cs

--- Lab7/White/Task2.cs
+++ Lab7/White/Task2.cs
@@ -4,45 +4,57 @@ namespace Lab7.White
 {
     public class Task2  
     {
-        public struct Participant
+public struct Participant
+{
+    private string name;
+    private string surname;
+    private double firstJump;
+    private double secondJump;
+
+    public string Name => name;
+    public string Surname => surname;
+    public double FirstJump => firstJump;
+    public double SecondJump => secondJump;
+
+    public double BestJump
+    {
+        get { return Math.Max(firstJump, secondJump); }
+    }
+
+    public Participant(string name, string surname)
+    {
+        this.name = name ?? "";
+        this.surname = surname ?? "";
+        firstJump = 0;
+        secondJump = 0;
+    }
+
+    public void Jump(double result)
+    {
+        if (firstJump == 0)
+            firstJump = result;
+        else if (secondJump == 0)
+            secondJump = result;
+    }
+
+    public static void Sort(Participant[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            private string name;
-            private string surname;
-            private double firstJump;
-            private double secondJump;
-
-            public string Name => name;
-            public string Surname => surname;
-            public double FirstJump => firstJump;
-            public double SecondJump => secondJump;
-
-            public double BestJump => Math.Max(firstJump, secondJump);
-
-            public Participant(string name, string surname)
+            for (int j = i + 1; j < array.Length; j++)
             {
-                this.name = name;
-                this.surname = surname;
-                firstJump = 0;
-                secondJump = 0;
-            }
-
-            public void Jump(double result)
-            {
-                if (firstJump == 0)
-                    firstJump = result;
-                else if (secondJump == 0)
-                    secondJump = result;
-            }
-
-            public static void Sort(Participant[] array)
-            {
-                Array.Sort(array, (a, b) => b.BestJump.CompareTo(a.BestJump));
-            }
-
-            public void Print()
-            {
-                Console.WriteLine($"{name} {surname} | 1-й: {firstJump} | 2-й: {secondJump} | Лучший: {BestJump}");
+                if (array[i].BestJump < array[j].BestJump)
+                {
+                    Participant temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{name} {surname} {BestJump}");
     }
 }
