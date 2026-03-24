4d39089656f035110fc9a0c677a33176611d899e
kolbin7750-max <kolbin7750@gmail.com>
2026-03-24 10:38:29 +0300

    Update Task1.cs

--- Lab7/White/Task1.cs
+++ Lab7/White/Task1.cs
@@ -1,6 +1,45 @@
-﻿namespace Lab7.White
+namespace Lab7.White
 {
     public class Task1
     {
+        public struct Participant      
+        {
+            private string surname;
+            private string club;
+            private double firstresult;
+            private double secondresult;
+
+            public Participant(string surname, string club) 
+            {
+                this.surname = surname;
+                this.club = club;
+            }
+
+            public void Jump(double result)
+            {
+                if (firstresult == 0)
+                {
+                    firstresult = result;
+                }
+                else if (secondresult == 0)
+                {
+                    secondresult = result;
+                }
+                
+            }
+
+            public double JumpSum => firstresult + secondresult;
+
+            public string Surname => surname;
+            public string Club => club;
+            public double FirstJump => firstresult;
+            public double SecondJump => secondresult;
+
+            public static void Sort(Participant[] array)
+            {
+                Array.Sort(array, (a, b) => b.JumpSum.CompareTo(a.JumpSum));
+            }
+            public void Print() { }
+        }
     }
 }
